154b6809ef50fce817884f454de75678a7c08f7a
Setsumiku <99953781+Setsumiku@users.noreply.github.com>
2022-07-24 13:03:40 +0200

    implement DAL

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Music_API.Data.Context;
+using Music_API.Data.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var connectionString = builder.Configuration.GetConnectionString("Music_API_Db");
 builder.Services.AddDbContext<Music_API_Context>(options => options.UseSqlServer(connectionString));
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
