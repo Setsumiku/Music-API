b223a4d9a7b40098fb86529c7f9a810e39dfe27f
Setsumiku <99953781+Setsumiku@users.noreply.github.com>
2022-07-30 18:05:11 +0200

    Implement HATEOAS in ArtistController

--- Controllers/MusicAPIArtistController.cs
+++ Controllers/MusicAPIArtistController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Music_API.Data.DAL;
 using Music_API.Data.Model;
+using Music_API.Entities;
 
 namespace Music_API.Controllers
 {
@@ -11,12 +12,14 @@ namespace Music_API.Controllers
         private readonly IMapper _mapper;
         private readonly ILogger _logger;
         private readonly IBaseRepository<Artist> _artistRepository;
+        private readonly LinkGenerator _linkGenerator;
 
-        public MusicAPIArtistController(IMapper mapper, ILogger<MusicAPIArtistController> logger, IBaseRepository<Artist> artistRepository)
+        public MusicAPIArtistController(IMapper mapper, LinkGenerator linkGenerator, ILogger<MusicAPIArtistController> logger, IBaseRepository<Artist> artistRepository)
         {
             _mapper = mapper;
             _logger = logger;
             _artistRepository = artistRepository;
+            _linkGenerator = linkGenerator;
         }
         /// <summary>
         /// Use to receive all Artists
@@ -25,7 +28,17 @@ namespace Music_API.Controllers
         // GET: api/<MusicAPIController>/artists
         [HttpGet("artists")]
         public async Task<IActionResult> Get()
-            => Ok(_mapper.Map<IEnumerable<ArtistReadDto>>(await _artistRepository.GetAllAsync(Array.Empty<string>())));
+        {
+            var artists = _mapper.Map<IEnumerable<ArtistReadDto>>(await _artistRepository.GetAllAsync(Array.Empty<string>()));
+            for (var index = 0; index < artists.Count(); index++)
+            {
+                artists.ElementAt(index).Add("Name", new { artists.ElementAt(index).ArtistDescription });
+                var artistLinks = CreateLinksForArtist(artists.ElementAt(index).ArtistId);
+                artists.ElementAt(index).Add("Links", artistLinks);
+            }
+            var artistsWrapper = new LinkCollectionWrapper<ArtistReadDto>(artists);
+            return Ok(CreateLinksForArtists(artistsWrapper));
+        }
 
         /// <summary>
         /// Use to receive specific Artist
@@ -37,8 +50,11 @@ namespace Music_API.Controllers
         public async Task<IActionResult> Get(int id)
         {
             var foundArtist = await _artistRepository.GetSingleByConditionAsync(artist => artist.ArtistId == id, Array.Empty<string>());
-            return foundArtist != null ? Ok(_mapper.Map<ArtistReadDto>(foundArtist))
-                                        : NotFound();
+            if (foundArtist is null) return NotFound();
+            var mappedArtist = _mapper.Map<ArtistReadDto>(foundArtist);
+            mappedArtist.Add("Name", new { mappedArtist.ArtistDescription });
+            mappedArtist.Add("Links", CreateLinksForArtist(foundArtist.ArtistId));
+            return Ok(mappedArtist);
         }
         /// <summary>
         /// Use to Create a new Artist
@@ -67,14 +83,14 @@ namespace Music_API.Controllers
         /// <returns>Updated Artist</returns>
         // PUT api/<MusicAPIController>/artists/{id}
         [HttpPut("artists/{id}")]
-        public async Task<IActionResult> Update(string id, [FromBody] ArtistReadDto artist)
+        public async Task<IActionResult> Update(string id, [FromBody] string artistName)
         {
             try
             {
                 var artistToUpdate = await _artistRepository.GetSingleByConditionAsync(artist => artist.ArtistId.ToString() == id, Array.Empty<string>());
                 if (artistToUpdate != null)
                 {
-                    artistToUpdate.ArtistDescription = artist.ArtistDescription;
+                    artistToUpdate.ArtistDescription = artistName;
                     _ = await _artistRepository.UpdateAsync(artistToUpdate);
                     return Ok();
                 }
@@ -105,5 +121,35 @@ namespace Music_API.Controllers
                 return NotFound();
             }
         }
+
+        private IEnumerable<Link> CreateLinksForArtist(int id)
+        {
+            var links = new List<Link>
+            {
+                new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(Get), values: new { id }),
+                "self",
+                "GET"),
+
+                new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(Delete), values: new { id }),
+                "remove_artist",
+                "DELETE"),
+
+                new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(Update), values: new { id }),
+                "edit_artist",
+                "PUT")
+            };
+            return links;
+        }
+        private LinkCollectionWrapper<ArtistReadDto> CreateLinksForArtists(LinkCollectionWrapper<ArtistReadDto> artistsWrapper)
+        {
+            artistsWrapper.Links.Add(new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(Get)),
+                    "self",
+                    "GET"));
+            artistsWrapper.Links.Add(new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(Add)),
+                "add_new_artist",
+                "POST"));
+            return artistsWrapper;
+        }
+
     }
 }
